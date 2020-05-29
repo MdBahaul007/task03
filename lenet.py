a3b0d6bb5c03a05b0e0fa750e09ab6619d085618
MdBahaul007 <faiz0581@gmail.com>
2020-05-29 12:54:24 +0530

    sixth

--- lenet.py
+++ lenet.py
@@ -69,7 +69,7 @@ model.compile(loss = 'categorical_crossentropy',
     
 print(model.summary())
 
-input_file = open('/task_03/input.txt','r')
+input_file = open('/root/task_03/input.txt','r')
 inputs = input_file.read()
 inputs = inputs.split('\n')
 epoch = int(inputs[0])
