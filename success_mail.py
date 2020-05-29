1c2592da445df82b741e073560979d3448be837f
MdBahaul007 <faiz0581@gmail.com>
2020-05-30 05:18:51 +0530

    success_mail.py
    first

---
+++ success_mail.py
@@ -0,0 +1,38 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+host_address = "your email address"
+host_pass = " *+*+*+*+*+ "
+guest_address = "guest email address"
+subject = "Regarding Success of your model "
+content = '''Hello, 
+	Dear Developer your model has achieved the desired accuracy.
+        And the attachment file has been send to you along with this mail.
+			THANK YOU ...'''
+message = MIMEMultipart()
+message['From'] = host_address
+message['To'] = guest_address
+message['Subject'] = subject
+
+message.attach(MIMEText(content, 'plain'))
+#for attaching and document
+from email.mime.base import MIMEBase 
+from email import encoders 
+filename = "input.txt"
+attachment = open('/root/mlopsactivity3/accuracy.txt', 'rb')
+p = MIMEBase('application', 'octet-stream')  
+p.set_payload((attachment).read()) 
+encoders.encode_base64(p) 
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename)  
+message.attach(p) 
+#attaching finished
+  
+
+#message.attach(MIMEText(content, 'plain'))
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.starttls()
+session.login(host_address, host_pass)
+text = message.as_string()
+session.sendmail(host_address, guest_address  , text)
+session.quit()
+print('Successfully sent your mail')
